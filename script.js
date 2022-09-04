59f04547d230f74c1fb63aed57805baca0a6fc46
lionofera <35335042+lionofera@users.noreply.github.com>
2022-09-04 12:55:37 +0200

    removed bug

--- script.js
+++ script.js
@@ -22,31 +22,31 @@
        const url = `https://openapi.programming-hero.com/api/news/category/${id}`
        fetch(url)
        .then(res => res.json())
-       .then(data => displayCategoryNews(data.data[0]))
+       .then(data => displayCategoryNews(data.data))
     }
 
     const displayCategoryNews = allnews => {
-        
-        const allNewsContainer = document.getElementById('all-news');
-        //console.log(allnews);
-
-        const newsDiv = document.createElement('div');
-        allNewsContainer.innerHTML = "";
-        newsDiv.classList.add('news');
-        newsDiv.innerHTML = `
+      const allNewsContainer = document.getElementById('all-news');
+      allNewsContainer.innerHTML = "";
+      allnews.forEach(news => {
+          const newsDiv = document.createElement('div');
+          newsDiv.classList.add('news');
+          newsDiv.innerHTML = `
         <div class="card mb-3" style="max-width: 540px;">
           <div class="row g-0">
             <div class="col-md-4">
-               <img src="${allnews.thumbnail_url? allnews.thumbnail_url : 'no image!'}" class="img-fluid rounded-start" alt="...">
+               <img src="${news.thumbnail_url? news.thumbnail_url : 'no image!'}" class="img-fluid rounded-start" alt="...">
             </div>
            <div class="col-md-8">
            <div class="card-body">
-              <h5 class="card-title">${allnews.title}</h5>
-              <p class="card-text text-truncate">${allnews.details.slice(0,250)}</p>
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text text-truncate">${news.details.slice(0,250)}</p>
               <div class="d-flex justify-content-center align-items-center space-around">
-              <img src="${allnews.author.img}" class="rounded-circle w-25" alt="...">
-              <h5>${allnews.author.name}</h5>
-              <p class=""><i class="fa-solid fa-eye"></i>${allnews.total_view}</p>
+                <img src="${news.author.img}" class="rounded-circle w-25" alt="...">
+                <h5>${news.author.name}</h5>
+                <p class=""><i class="fa-solid fa-eye"></i>${news.total_view}</p>
+                <button onclick = "showMore()"><i class="fa-solid fa-angles-right"></i></button>
+
               </div>
            </div>
           </div>
@@ -54,7 +54,11 @@
         </div>
         `;
         allNewsContainer.appendChild(newsDiv);
+      })
   
+      }
+      const showMore = () => {
+         
       }
 
     loadData()
